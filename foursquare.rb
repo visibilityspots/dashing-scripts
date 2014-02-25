c857db8101639f51da54c866c6e5f0a27651438b
Jan Collijs <jan.collijs@inuits.eu>
2014-02-25 09:41:40 +0100

    Bugfix: foursquare API needs version from now on

--- foursquare.rb
+++ foursquare.rb
@@ -3,10 +3,11 @@ require 'foursquare2'
 
 _api_client_id = ''
 _api_client_secret = ''
+_api_version = 20140225
 _venue_id = ''
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
-  venue = Foursquare2::Client.new(:client_id => _api_client_id, :client_secret => _api_client_secret)
+  venue = Foursquare2::Client.new(:client_id => _api_client_id, :client_secret => _api_client_secret, :api_version => _api_version)
   venue_checkins = venue.herenow(_venue_id)
   send_event('foursquare_checkins_people', current: venue_checkins['hereNow']['count'])
 end
